42df73108977d78e50dc15d5c2cf453114efa344
davidxiong <>
2021-06-22 13:50:19 +0800

    数字组合

--- demo/agg_num.py
+++ demo/agg_num.py
@@ -4,21 +4,21 @@
 程序分析 遍历全部可能，把有重复的剃掉。"""
 
 # 第一种方式
-total=0
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if ((i!=j)and(j!=k)and(k!=i)):
-                            print(i,j,k)
-                            total+=1
+total = 0
+for i in range(1, 5):
+    for j in range(1, 5):
+        for k in range(1, 5):
+            if ((i != j) and (j != k) and (k != i)):
+                            print(i, j, k)
+                            total += 1
 print(total)
 
 
 # 第二种方式
 import itertools
-sum2=0
-a=[1,2,3,4]
-for i in itertools.permutations(a,3):
+sum2 = 0
+a = [1, 2, 3, 4]
+for i in itertools.permutations(a, 3):
     print(i)
-    sum2+=1
+    sum2 += 1
 print(sum2)
