fd4d47bf781c84435e908ee56eb0a241b26b5425
xiongyangyang_ext <xiongyangyang_ext@cop.cmsoft.com.cn>
2021-07-04 08:16:41 +0800

    db

---
+++ db/db.py
@@ -0,0 +1,14 @@
+import pymysql
+import redis
+from . import config
+
+from flask import g
+
+def get_db():
+    if 'db' not in g:
+        g.db = pymysql.connect(host=config['DATABASE_HOST'], port=config['DATABASE_PORT'],
+                               user=config['DATABASE_USER'],
+                               password=config['DATABASE_PASSWORD'],
+                               db=config['DATABASE_DB'], charset=config['DATABASE_CHARSET'])
+
+    return g.db
---
+++ db/f_db.py
@@ -0,0 +1,59 @@
+from pymysql import cursors
+
+from db.db import get_db
+
+# 导入get_db方法
+db = get_db()
+
+def add_db(params):
+
+    # insert
+    try:
+        cursor = db.cursor(cursor=cursors.DictCursor)
+        cursor.execute('insert into  person value(%s,%s,%s)',
+                       (params['id'], params['name'], params['age']
+                        ))
+    except Exception as e:
+        db.rollback()
+        return False
+    else:
+        db.commit()
+        return True
+
+def update_db(params, id):
+    # update
+    try:
+        cursor = db.cursor(cursor=cursors.DictCursor)
+        cursor.execute('update person set name=%s,age=%s where id=%s',
+                       (params['name'], params['age'],id
+                        ))
+    except Exception as e:
+        db.rollback()
+        return False
+    else:
+        db.commit()
+        return True
+
+
+def get_db(id):
+
+    # get
+    cursor = db.cursor(cursor=cursors.DictCursor)
+    cursor.execute("select * from person where id=%s", id)
+    # cursor.execute("select * from person")
+    person_info = cursor.fetchone()
+    # person_list = cursor.fetchall()
+    return person_info
+
+
+def delete_db(id):
+    # delete
+    try:
+        cursor = db.cursor(cursor=cursors.DictCursor)
+        cursor.execute("delete from person where id=%s", id)
+    except Exception as e:
+        db.rollback()
+        return False
+    else:
+        db.commit()
+        return True
