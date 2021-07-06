010803b1ec01bc52dac490f887e98fb7d4bc396f
Kepler-XX <xyy1054997454@163.com>
2021-07-06 18:30:26 +0800

    selenium test

--- spider/selinum_test.py
+++ spider/selinum_test.py
@@ -7,8 +7,15 @@ url = "http://liuyan.people.com.cn/"
 
 browser = webdriver.Chrome()
 browser.get(url)
+sleep(1)
 
-sleep(3)
-browser.find_element_by_class_name("fl")
+browser.find_element_by_link_text("留言").click()
+sleep(1)
+
+browser.find_element_by_class_name("nav_active").click()
+sleep(1)
+
+browser.find_element_by_id("13").click()
+
 
 browser.close()
