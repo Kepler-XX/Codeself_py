0358b71212cab0c4b127799db91ac52c21cd1cfc
Kepler-XX <xyy1054997454@163.com>
2021-08-14 17:49:08 +0800

    测试网速大小（不成功）

---
+++ spider/httpx/speedtest_.py
@@ -0,0 +1,25 @@
+"""
+Speedtest模块可以测试电脑的网络带宽大小。
+安装
+使用百度源安装库。
+pip install speedtest -i https://mirror.baidu.com/pypi/simple/
+
+使用时，需要取消证书验证。
+"""
+
+# import speedtest
+#
+#
+# # 全局取消证书验证
+# import ssl
+# ssl._create_default_https_context = ssl.create_default_context
+#
+# test = speedtest.Speedtest()
+# down = test.download()
+# upload = test.upload()
+
+
+# print(f"上传速度：{round(upload/(1024 * 1024),2)} Mbps")
+# print(f"下载速度：{round(down/(1024 * 1024),2)} Mbps")
+
+
